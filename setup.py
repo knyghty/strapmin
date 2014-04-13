8619faf6808b2a93bee157677be41abb455a5991
Tom Carrick <tom@calmdigital.com>
2014-04-13 21:09:53 +0100

    Add files

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup
 
 
-setup(name='strapmin',
-    version='0.1',
+version = '0.1'
+
+setup(
+    name='strapmin',
+    version=version,
     packages=['strapmin'],
     include_package_data=True,
+    author='Tom Carrick',
+    author_email='knyght@knyg.ht',
+    license='BSD 2 Clause',
+    long_description='A bootstrappy django admin reskin',
+    description='A bootstrappy django admin reskin',
+    install_requires=['django>=1.5'],
+    requires=['django (>=1.5)'],
 )
