02b688bbf4c738abf22c354da434baad967623b9
Tom Carrick <knyght@gmail.com>
2014-07-16 13:46:25 +0100

    Fix ckeditor in admin inlines

---
+++ strapmin/static/admin/js/ckeditor/jquery-ckeditor.js
@@ -0,0 +1,17 @@
+django.jQuery(function(){
+    function init_cke(){
+    	$('textarea[data-type=ckeditortype]').each(function(){
+        	if($(this).data('processed') == "0" && $(this).attr('id').indexOf('__prefix__') == -1){
+	            $(this).data('processed',"1");
+	            CKEDITOR.replace($(this).attr('id'));
+	        }
+	    });
+    }
+
+    init_cke();
+
+    $(".add-row a").click(function(){
+		init_cke();
+		return true;
+	});
+});
--- strapmin/widgets.py
+++ strapmin/widgets.py
@@ -1,10 +1,25 @@
 from django import forms
+from django.forms.util import flatatt
+from django.template.loader import render_to_string
+from django.utils.encoding import force_text
+from django.utils.safestring import mark_safe
 
 
 class RichTextEditorWidget(forms.Textarea):
-    def __init__(self, *args, **kwargs):
-        kwargs['attrs'] = {'class': 'ckeditor'}
-        super(RichTextEditorWidget, self).__init__(*args, **kwargs)
+    #def __init__(self, *args, **kwargs):
+    #    kwargs['attrs'] = {'class': 'ckeditor'}
+    #    super(RichTextEditorWidget, self).__init__(*args, **kwargs)
+
+    def render(self, name, value, attrs={}):
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, name=name)
+        return mark_safe(render_to_string('ckeditor/widget.html', {
+            'final_attrs': flatatt(final_attrs),
+            'value': force_text(value),
+            'id': final_attrs['id'],
+        }))
 
     class Media:
-        js = ('admin/js/ckeditor/ckeditor.js',)
+        js = ('admin/js/ckeditor/ckeditor.js',
+              'admin/js/ckeditor/jquery-ckeditor.js')
